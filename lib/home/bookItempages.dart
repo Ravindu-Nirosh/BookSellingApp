42b26b376e4b06ef0b6c51b60a5bc9dad2763a74
Ravindu <ravindud621@gmail.com>
2022-10-02 22:21:25 +0530

    Second Commit (Dot After Dot Indicators)

--- lib/home/bookItempages.dart
+++ lib/home/bookItempages.dart
@@ -1,5 +1,6 @@
 import 'package:booksellapp/utils/appcolor.dart';
 import 'package:booksellapp/widgets/MainText.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/iconandtext.dart';
@@ -14,140 +15,214 @@ class BookItemsPages extends StatefulWidget {
 
 class _BookItemsPagesState extends State<BookItemsPages> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  final double _scaleFactor = 0.8;
+  final double _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 320,
-      child: PageView.builder(
-        controller: pageController,
-        itemCount: 5,
-        itemBuilder: (context, position) {
-          return _buildPageItems(position);
-        },
-      ),
+    return Column(
+      children: [
+        Container(
+          height: 300,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, position) {
+              return _buildPageItems(position);
+            },
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        DotsIndicator(
+          dotsCount: 5,
+          position: _currentPageValue,
+          decorator: DotsDecorator(
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            color: AppColor.mainIconBgColor,
+            activeColor: AppColor.dotActiveColor,
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+          ),
+        ),
+      ],
     );
   }
 
   Widget _buildPageItems(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: const EdgeInsets.only(
-            left: 5,
-            right: 5,
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven
-                ? const Color(0xFF69c5df)
-                : const Color(0xff9294cc),
-            image: const DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/images/cardImage1.jpg"),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
+    Matrix4 matrix = Matrix4.identity();
+    if (index == _currentPageValue.floor()) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var curScale = _scaleFactor;
+
+      var currTrans = _height * (1 - curScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, curScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
             margin: const EdgeInsets.only(
-              left: 30,
-              right: 30,
-              bottom: 10,
+              left: 5,
+              right: 5,
             ),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: const Color(0xff1e2023),
-            ),
-            child: Container(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  MainTexts(displayText: 'Display Text'),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(
-                            5,
-                            (index) => const Icon(
-                                  Icons.star,
-                                  size: 15,
-                                  color: AppColor.mainIconBgColor,
-                                )),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Opacity(
-                        opacity: 0.5,
-                        child: SmallText(
-                          displayText: '4.5',
-                          color: Colors.grey,
-                          size: 12,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Opacity(
-                        opacity: 0.5,
-                        child: SmallText(
-                          displayText: '1234',
-                          color: Colors.grey,
-                          size: 12,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Opacity(
-                        opacity: 0.5,
-                        child: SmallText(
-                          displayText: 'Comments',
-                          color: Colors.grey,
-                          size: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      IconAndTextWidget(
-                        text: 'Normal',
-                        color: AppColor.smallTextColor,
-                        icon: Icons.circle,
-                        iconColor: AppColor.mainIconBgColor,
-                      ),
-                      IconAndTextWidget(
-                        text: '1.5km',
-                        color: AppColor.smallTextColor,
-                        icon: Icons.location_pin,
-                        iconColor: AppColor.mainIconBgColor,
-                      ),
-                      IconAndTextWidget(
-                        text: 'Normal',
-                        color: AppColor.smallTextColor,
-                        icon: Icons.location_city,
-                        iconColor: AppColor.mainIconBgColor,
-                      ),
-                    ],
-                  ),
-                ],
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven
+                  ? const Color(0xFF69c5df)
+                  : const Color(0xff9294cc),
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/cardImage1.jpg"),
               ),
             ),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: const EdgeInsets.only(
+                left: 30,
+                right: 30,
+                bottom: 10,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color(0xff1e2023),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5),
+                  )
+                ],
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    MainTexts(displayText: 'Display Text'),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                              5,
+                              (index) => const Icon(
+                                    Icons.star,
+                                    size: 15,
+                                    color: AppColor.mainIconBgColor,
+                                  )),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Opacity(
+                          opacity: 0.5,
+                          child: SmallText(
+                            displayText: '4.5',
+                            color: Colors.grey,
+                            size: 12,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Opacity(
+                          opacity: 0.5,
+                          child: SmallText(
+                            displayText: '1234',
+                            color: Colors.grey,
+                            size: 12,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Opacity(
+                          opacity: 0.5,
+                          child: SmallText(
+                            displayText: 'Comments',
+                            color: Colors.grey,
+                            size: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        IconAndTextWidget(
+                          text: 'Normal',
+                          color: AppColor.smallTextColor,
+                          icon: Icons.circle,
+                          iconColor: AppColor.iconColor1,
+                        ),
+                        IconAndTextWidget(
+                          text: '1.5km',
+                          color: AppColor.smallTextColor,
+                          icon: Icons.location_pin,
+                          iconColor: AppColor.iconColor2,
+                        ),
+                        IconAndTextWidget(
+                          text: 'Normal',
+                          color: AppColor.smallTextColor,
+                          icon: Icons.location_city,
+                          iconColor: AppColor.iconColor3,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/utils/appcolor.dart
+++ lib/utils/appcolor.dart
@@ -4,4 +4,8 @@ class AppColor {
   static const Color mainText = Color(0xFFAAC4FF);
   static const Color mainIconBgColor = Color(0xFF89dad0);
   static const Color smallTextColor = Color(0xff808080);
+  static const Color iconColor1 = Color(0xffffcdb2);
+  static const Color iconColor2 = Color(0xffe9c46a);
+  static const Color iconColor3 = Color(0xfff4a261);
+  static const Color dotActiveColor = Color(0xff144552);
 }
